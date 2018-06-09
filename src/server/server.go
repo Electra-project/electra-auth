d95f2cd5d17b656bec47bd59a082db952018c06b
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-09 09:23:42 +0200

    server/server: remove .env injection

--- src/server/server.go
+++ src/server/server.go
@@ -2,19 +2,10 @@ package server
 
 import (
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 // Start the server.
 func Start() {
-	if os.Getenv("GO_ENV") != "production" {
-		err := godotenv.Load()
-		if err != nil {
-			panic(err)
-		}
-	}
-
 	router := Router()
 	router.Run(":" + getPort())
 }
