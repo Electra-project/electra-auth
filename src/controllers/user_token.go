ddc83f92dc9dcb06217f84ef2d5bcf6297623f8c
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-09 23:30:08 +0200

    controllers/user_token: create

---
+++ src/controllers/user_token.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"github.com/Electra-project/electra-auth/src/models"
+	"github.com/gin-gonic/gin"
+)
+
+// UserTokenController class.
+type UserTokenController struct{}
+
+var userTokenModel = new(models.UserToken)
+
+// Get a user token chanllenge from a Purse Account address hash.
+func (u UserTokenController) Get(c *gin.Context) {
+	purseHash := c.Param("purseHash")
+
+	if !isPurseHashValid(purseHash) {
+		c.JSON(400, gin.H{"message": "Invalid Purse Account address hash."})
+		c.Abort()
+
+		return
+	}
+
+	user, err := userTokenModel.GetByPurseHash(c.Param("purseHash"))
+	if err != nil {
+		c.JSON(500, gin.H{"message": "Internal Server Error."})
+		c.Abort()
+
+		return
+	}
+
+	c.JSON(200, gin.H{"user": user})
+
+	return
+}
+
+func isPurseHashValid(purseHash string) bool {
+	return len(purseHash) == 34 && string([]rune(purseHash)[0]) == "E"
+}
