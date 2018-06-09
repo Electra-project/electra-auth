cebf1f65eb4ef0fa9dc4605ead737f2492419a58
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-09 09:23:28 +0200

    main: add .env injection

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Electra-project/electra-auth/src/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if os.Getenv("GO_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	server.Start()
 }
