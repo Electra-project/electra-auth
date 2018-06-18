c13f35c1359449e26690ddea3a7d98087b878ff1
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-18 08:10:06 +0200

    controllers/status: replace model by new rpc lib

--- src/controllers/status.go
+++ src/controllers/status.go
@@ -1,18 +1,16 @@
 package controllers
 
 import (
-	"github.com/Electra-project/electra-auth/src/models"
+	"github.com/Electra-project/electra-auth/src/libs/rpc"
 	"github.com/gin-gonic/gin"
 )
 
 // StatusController describes a status controller.
 type StatusController struct{}
 
-var statusModel = new(models.Status)
-
 // Get a user public data.
 func (s StatusController) Get(c *gin.Context) {
-	status, err := statusModel.Get()
+	status, err := rpc.GetInfo()
 	if err != nil {
 		c.JSON(500, gin.H{"message": "Internal Server Error."})
 		c.Abort()
