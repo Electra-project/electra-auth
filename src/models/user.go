9096773e6d0c48c6e339deb3b1b8c6e24ae7f80c
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-18 20:39:23 +0200

    models/user: add token generation in Insert() method

--- src/models/user.go
+++ src/models/user.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/Electra-project/electra-auth/src/database"
+	"github.com/Electra-project/electra-auth/src/helpers"
 	"github.com/globalsign/mgo/bson"
 )
 
@@ -12,6 +13,7 @@ import (
 type User struct {
 	ID               bson.ObjectId   `bson:"_id" json:"-"`
 	PurseHash        string          `bson:"purseHash" json:"purseHash"`
+	Token            string          `bson:"token" json:"token"`
 	PursePrivateKey  string          `bson:"pursePrivateKey" json:"-"`
 	TwitterUsername  string          `bson:"twitterUsername" json:"twitterUsername"`
 	TwitterCheckedAt time.Time       `bson:"twitterCheckedAt" json:"twitterCheckedAt"`
@@ -19,6 +21,8 @@ type User struct {
 	CreatedAt        time.Time       `bson:"createdAt" json:"createdAt"`
 	UpdatedAt        time.Time       `bson:"updatedAt" json:"updatedAt"`
 }
+
+const tokenLength uint8 = 196
 
 // GetByPurseHash gets a user from the database
 // by their Purse Account address hash.
@@ -39,16 +43,21 @@ func (u User) GetByPurseHash(purseHash string) (*User, error) {
 
 // Insert creates a new user in the database.
 func (u User) Insert(purseHash string) (*User, error) {
+	token, err := helpers.GenerateToken(tokenLength)
+	if err != nil {
+		return nil, err
+	}
+
 	db := database.Get()
 	collection := db.C("users")
 
-	err := collection.Insert(bson.M{
+	err = collection.Insert(bson.M{
 		"purseHash": purseHash,
+		"token":     token,
 		"createdAt": time.Now(),
 		"updatedAt": time.Now(),
 	})
 	if err != nil {
-		println(err)
 		return nil, err
 	}
 
