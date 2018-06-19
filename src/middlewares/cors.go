825863d4061412b9af30d5e614a0055e3fe83f94
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-19 09:06:55 +0200

    middlewares/cors: add Access-Control-Allow-Headers

--- src/middlewares/cors.go
+++ src/middlewares/cors.go
@@ -11,6 +11,7 @@ func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Accept, Accept-Encoding, Authorization, Content-Length, Content-Type, Origin")
 		c.Header("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, POST, PUT")
 
 		if c.Request.Method == "OPTIONS" {
