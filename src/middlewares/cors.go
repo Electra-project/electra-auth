784ce2a3d835ea1fd9958ea055d36e2071d18c05
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-19 07:13:41 +0200

    middlewares/cors: accept all OPTIONS requests

--- src/middlewares/cors.go
+++ src/middlewares/cors.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,5 +10,10 @@ import (
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
+		// c.Header("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+		}
 	}
 }
