cd414d97b13fe1a52719e83550b61b7ebb84b116
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-22 19:14:21 +0200

    server/router: remove version in path

--- src/server/router.go
+++ src/server/router.go
@@ -11,32 +11,29 @@ func Router() *gin.Engine {
 	router := gin.Default()
 	router.Use(middlewares.Cors())
 
-	v1 := router.Group("v1")
-	{
-		v1.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"name":    "Electra Auth API",
-				"version": "1",
-			})
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"name":    "Electra Auth API",
+			"version": "1",
 		})
+	})
 
-		statusController := new(controllers.StatusController)
-		v1.GET("/status", statusController.Get)
+	statusController := new(controllers.StatusController)
+	router.GET("/status", statusController.Get)
 
-		userGroup := v1.Group("user")
-		{
-			userTokenController := new(controllers.UserTokenController)
-			userGroup.GET("/:purseHash/token", userTokenController.Get)
-			userGroup.POST("/:purseHash/token", userTokenController.Post)
-		}
+	userGroup := router.Group("user")
+	{
+		userTokenController := new(controllers.UserTokenController)
+		userGroup.GET("/:purseHash/token", userTokenController.Get)
+		userGroup.POST("/:purseHash/token", userTokenController.Post)
+	}
 
-		v1.Use(middlewares.IsUser())
-		{
-			userController := new(controllers.UserController)
-			v1.GET("/user", userController.Get)
-			v1.POST("/user", userController.Post)
-			v1.PUT("/user", userController.Put)
-		}
+	router.Use(middlewares.IsUser())
+	{
+		userController := new(controllers.UserController)
+		router.GET("/user", userController.Get)
+		router.POST("/user", userController.Post)
+		router.PUT("/user", userController.Put)
 	}
 
 	router.NoRoute(func(c *gin.Context) {
